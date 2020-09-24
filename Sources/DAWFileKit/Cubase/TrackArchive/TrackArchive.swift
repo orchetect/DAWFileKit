df9c6c9fc11035cf47e61cf0bcf21951645ceb82
Steffan Andrews <stef@orchetect.com>
2020-09-23 23:13:18 -0700

    Added init with arguments to Cubase.TrackArchive.CycleMarker

--- Sources/DAWFileKit/Cubase/TrackArchive/TrackArchive.swift
+++ Sources/DAWFileKit/Cubase/TrackArchive/TrackArchive.swift
@@ -79,36 +79,45 @@ extension Cubase.TrackArchive {
 		
 	}
 	
-	internal static var FrameRateTable: [Int : Timecode.FrameRate] = [2 : ._24,
-																		3 : ._25,
-																		4 : ._29_97,
-																		5 : ._30,
-																		6 : ._29_97_drop,
-																		7 : ._30_drop,
-																		12 : ._23_976,
-																		13 : ._24_98,
-																		14 : ._50,
-																		15 : ._59_94,
-																		16 : ._60]
-		
+	internal static var FrameRateTable: [Int : Timecode.FrameRate] =
+		[
+			02 : ._24,
+			03 : ._25,
+			04 : ._29_97,
+			05 : ._30,
+			06 : ._29_97_drop,
+			07 : ._30_drop,
+			12 : ._23_976,
+			13 : ._24_98,
+			14 : ._50,
+			15 : ._59_94,
+			16 : ._60
+		]
+	
 	public enum TimeType: Int {
+		
 		case secondsOrBarsAndBeats = 0 // seconds and bars+beats both show up as 0
 		case timecode = 4 // and 13?
 		case samples = 10
 		case user = 11
+		
 	}
 	
 }
 
 public protocol CubaseTrackArchiveTrack {
+	
 	var name: String? { get set }
+	
 }
 
 public protocol CubaseTrackArchiveMarker {
+	
 	var name: String { get set }
 	
 	var startTimecode: Timecode { get set }
 	var startRealTime: TimeValue? { get set }
+	
 }
 
 extension Cubase.TrackArchive {
@@ -117,40 +126,54 @@ extension Cubase.TrackArchive {
 		["MMarkerTrackEvent" : MarkerTrack.self]
 	
 	public enum TrackTimeDomain: Int {
+		
 		/// Bars & beats timebase - computations are against PPQ base and tempo
 		case musical = 0
 		/// Time linear timebase (real / absolute time)
 		case linear = 1
+		
 	}
 	
 	/// Represents a marker event and its contents
 	public struct Marker: CubaseTrackArchiveMarker {
+		
 		public var name: String = ""
 		
 		public var startTimecode: Timecode
 		public var startRealTime: TimeValue?
 		
-		public init(name: String, startTimecode: Timecode, startRealTime: TimeValue? = nil) {
+		public init(name: String,
+					startTimecode: Timecode,
+					startRealTime: TimeValue? = nil)
+		{
+			
 			self.name = name
+			
 			self.startTimecode = startTimecode
 			self.startRealTime = startRealTime
+			
 		}
+		
 	}
 	
 	/// Represents a cycle marker event and its contents
 	public struct TempoTrack: CubaseTrackArchiveTrack {
+		
 		public var name: String?
 		
 		public var events: [Event] = []
 		
 		public struct Event {
+			
 			public var startTimeAsPPQ: Double
 			public var tempo: Double
 			public var type: TempoEventType
 			
 			public enum TempoEventType {
+				
 				case jump
 				case ramp
+				
 			}
 		}
 		
@@ -158,6 +181,7 @@ extension Cubase.TrackArchive {
 	
 	/// Represents a cycle marker event and its contents
 	public struct CycleMarker: CubaseTrackArchiveMarker {
+		
 		public var name: String = ""
 		
 		public var startTimecode: Timecode
@@ -165,21 +189,43 @@ extension Cubase.TrackArchive {
 		
 		public var lengthTimecode: Timecode
 		public var lengthRealTime: TimeValue?
+		
+		public init(name: String,
+					startTimecode: Timecode,
+					startRealTime: TimeValue? = nil,
+					lengthTimecode: Timecode,
+					lengthRealTime: TimeValue? = nil)
+		{
+			
+			self.name = name
+			
+			self.startTimecode = startTimecode
+			self.startRealTime = startRealTime
+			
+			self.lengthTimecode = lengthTimecode
+			self.lengthRealTime = lengthRealTime
+			
+		}
+		
 	}
 	
 	/// Represents a track and its contents
 	public struct MarkerTrack: CubaseTrackArchiveTrack {
+		
 		public var name: String?
 		
 		public var events: [CubaseTrackArchiveMarker] = []
 		
 		public init() { }
+		
 	}
 	
 	public struct OrphanTrack: CubaseTrackArchiveTrack {
+		
 		public var name: String?
 		
 		public let rawXMLContent: String
+		
 	}
 	
 }
