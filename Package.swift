50e5ed0a6270ecd317c18e50d94fe1271cac17da
Steffan Andrews <stef@orchetect.com>
2021-11-13 18:28:55 -0800

    Bumped SPM dependency versions

--- Package.swift
+++ Package.swift
@@ -6,17 +6,19 @@ let package = Package(
 	
 	name: "DAWFileKit",
 	
-    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v6)],
-	
-	products: [
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v6)
+    ],
+    
+    products: [
 		.library(
 			name: "DAWFileKit",
 			targets: ["DAWFileKit"])
 	],
 	
 	dependencies: [
-		.package(url: "https://github.com/orchetect/OTCore", from: "1.1.14"),
-		.package(url: "https://github.com/orchetect/TimecodeKit", from: "1.2.3")
+		.package(url: "https://github.com/orchetect/OTCore", from: "1.1.18"),
+		.package(url: "https://github.com/orchetect/TimecodeKit", from: "1.2.6")
 	],
 	
 	targets: [
@@ -30,7 +32,7 @@ let package = Package(
 			resources: [
                 .copy("Cubase/Resources/Cubase TrackArchive XML"),
                 .copy("Pro Tools/Resources/PT Session Text Exports")
-            ]),
+            ])
 	]
 	
 )
