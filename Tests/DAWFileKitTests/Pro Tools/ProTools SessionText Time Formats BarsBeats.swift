f2c8b02de63938e21e0fe17b74eb524da6a36e6d
Steffan Andrews <stef@orchetect.com>
2022-10-06 01:13:44 -0700

    Updated unit tests

--- Tests/DAWFileKitTests/Pro Tools/ProTools SessionText Time Formats BarsBeats.swift
+++ Tests/DAWFileKitTests/Pro Tools/ProTools SessionText Time Formats BarsBeats.swift
@@ -14,9 +14,76 @@ class ProTools_SessionText_TimeFormats_BarsBeats: XCTestCase {
     override func tearDown() { }
     
     func testSessionText_BarsBeats() throws {
-        // load file
-        
-        let filename = "SessionText_TimeFormats_BarsBeats_PT2022.9"
+        try runBarsBeats(
+            filename: "SessionText_TimeFormats_BarsBeats_PT2022.9",
+            track1Clips: [
+                .init(
+                    channel: 1,
+                    event: 1,
+                    name: "Audio Clip 1 Name",
+                    startTime: .barsAndBeats(bar: 13, beat: 3, ticks: nil),
+                    endTime: .barsAndBeats(bar: 20, beat: 1, ticks: nil),
+                    duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
+                    state: .unmuted
+                )
+            ], track2Clips: [
+                .init(
+                    channel: 1,
+                    event: 1,
+                    name: "Audio Clip 2 Name",
+                    startTime: .barsAndBeats(bar: 5, beat: 1, ticks: nil),
+                    endTime: .barsAndBeats(bar: 11, beat: 4, ticks: nil),
+                    duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
+                    state: .unmuted
+                )
+            ],
+            marker1Location: .barsAndBeats(bar: 29, beat: 1, ticks: nil),
+            marker1TimeRef: .samples(2695168),
+            marker2Location: .barsAndBeats(bar: 58, beat: 4, ticks: nil),
+            marker2TimeRef: .barsAndBeats(bar: 58, beat: 4, ticks: nil)
+        )
+    }
+    
+    func testSessionText_BarsBeats_ShowSubframes() throws {
+        try runBarsBeats(
+            filename: "SessionText_TimeFormats_BarsBeats_ShowSubframes_PT2022.9",
+            track1Clips: [
+                .init(
+                    channel: 1,
+                    event: 1,
+                    name: "Audio Clip 1 Name",
+                    startTime: .barsAndBeats(bar: 13, beat: 3, ticks: 48),
+                    endTime: .barsAndBeats(bar: 20, beat: 1, ticks: 768),
+                    duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
+                    state: .unmuted
+                )
+            ], track2Clips: [
+                .init(
+                    channel: 1,
+                    event: 1,
+                    name: "Audio Clip 2 Name",
+                    startTime: .barsAndBeats(bar: 5, beat: 1, ticks: 696),
+                    endTime: .barsAndBeats(bar: 11, beat: 4, ticks: 456),
+                    duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
+                    state: .unmuted
+                )
+            ],
+            marker1Location: .barsAndBeats(bar: 29, beat: 1, ticks: 287),
+            marker1TimeRef: .samples(2695168),
+            marker2Location: .barsAndBeats(bar: 58, beat: 4, ticks: 735),
+            marker2TimeRef: .barsAndBeats(bar: 58, beat: 4, ticks: 735)
+        )
+    }
+    
+    func runBarsBeats(
+        filename: String,
+        track1Clips: [ProTools.SessionInfo.Track.Clip],
+        track2Clips: [ProTools.SessionInfo.Track.Clip],
+        marker1Location: ProTools.SessionInfo.TimeValue,
+        marker1TimeRef: ProTools.SessionInfo.TimeValue,
+        marker2Location: ProTools.SessionInfo.TimeValue,
+        marker2TimeRef: ProTools.SessionInfo.TimeValue
+    ) throws {
         guard let rawData = loadFileContents(
             forResource: filename,
             withExtension: "txt",
@@ -81,17 +148,7 @@ class ProTools_SessionText_TimeFormats_BarsBeats: XCTestCase {
         XCTAssertEqual(track1.userDelay, 0)
         XCTAssertEqual(track1.state, [])
         XCTAssertEqual(track1.plugins, [])
-        XCTAssertEqual(track1.clips, [
-            .init(
-                channel: 1,
-                event: 1,
-                name: "Audio Clip 1 Name",
-                startTime: .barsAndBeats(bar: 13, beat: 3, ticks: nil),
-                endTime: .barsAndBeats(bar: 20, beat: 1, ticks: nil),
-                duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
-                state: .unmuted
-            )
-        ])
+        XCTAssertEqual(track1.clips, track1Clips)
         
         let track2 = try XCTUnwrap(tracks[safe: 1])
         XCTAssertEqual(track2.name, "Audio B")
@@ -99,22 +156,26 @@ class ProTools_SessionText_TimeFormats_BarsBeats: XCTestCase {
         XCTAssertEqual(track2.userDelay, 0)
         XCTAssertEqual(track2.state, [])
         XCTAssertEqual(track2.plugins, [])
-        XCTAssertEqual(track2.clips, [
-            .init(
-                channel: 1,
-                event: 1,
-                name: "Audio Clip 2 Name",
-                startTime: .barsAndBeats(bar: 5, beat: 1, ticks: nil),
-                endTime: .barsAndBeats(bar: 11, beat: 4, ticks: nil),
-                duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
-                state: .unmuted
-            )
-        ])
+        XCTAssertEqual(track2.clips, track2Clips)
         
         // markers
         
         let markers = try XCTUnwrap(sessionInfo.markers)
         XCTAssertEqual(markers.count, 2)
+        
+        let marker1 = try XCTUnwrap(markers[safe: 0])
+        XCTAssertEqual(marker1.number, 1)
+        XCTAssertEqual(marker1.location, marker1Location)
+        XCTAssertEqual(marker1.timeReference, marker1TimeRef)
+        XCTAssertEqual(marker1.name, "Marker Absolute")
+        XCTAssertEqual(marker1.comment, "Comment")
+        
+        let marker2 = try XCTUnwrap(markers[safe: 1])
+        XCTAssertEqual(marker2.number, 2)
+        XCTAssertEqual(marker2.location, marker2Location)
+        XCTAssertEqual(marker2.timeReference, marker2TimeRef)
+        XCTAssertEqual(marker2.name, "Marker Bars-Beats")
+        XCTAssertEqual(marker2.comment, "Comment")
         
         // orphan data
         
