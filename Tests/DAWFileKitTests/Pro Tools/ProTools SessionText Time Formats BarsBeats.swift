4fa068a5ac7fe318d08fc22948e0e45ad241794f
Steffan Andrews <stef@orchetect.com>
2022-10-06 01:55:18 -0700

    Updated unit tests

--- Tests/DAWFileKitTests/Pro Tools/ProTools SessionText Time Formats BarsBeats.swift
+++ Tests/DAWFileKitTests/Pro Tools/ProTools SessionText Time Formats BarsBeats.swift
@@ -9,76 +9,226 @@ import XCTest
 import OTCore
 import TimecodeKit
 
-class ProTools_SessionText_TimeFormats_BarsBeats: XCTestCase {
+class ProTools_SessionText_TimeFormats: XCTestCase {
     override func setUp() { }
     override func tearDown() { }
     
     func testSessionText_BarsBeats() throws {
-        try runBarsBeats(
+        try runSessionText(
             filename: "SessionText_TimeFormats_BarsBeats_PT2022.9",
-            track1Clips: [
-                .init(
-                    channel: 1,
-                    event: 1,
-                    name: "Audio Clip 1 Name",
-                    startTime: .barsAndBeats(bar: 13, beat: 3, ticks: nil),
-                    endTime: .barsAndBeats(bar: 20, beat: 1, ticks: nil),
-                    duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
-                    state: .unmuted
-                )
-            ], track2Clips: [
-                .init(
-                    channel: 1,
-                    event: 1,
-                    name: "Audio Clip 2 Name",
-                    startTime: .barsAndBeats(bar: 5, beat: 1, ticks: nil),
-                    endTime: .barsAndBeats(bar: 11, beat: 4, ticks: nil),
-                    duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
-                    state: .unmuted
-                )
-            ],
-            marker1Location: .barsAndBeats(bar: 29, beat: 1, ticks: nil),
-            marker1TimeRef: .samples(2695168),
-            marker2Location: .barsAndBeats(bar: 58, beat: 4, ticks: nil),
-            marker2TimeRef: .barsAndBeats(bar: 58, beat: 4, ticks: nil)
+            
+            track1ClipStartTime: .barsAndBeats(bar: 13, beat: 3, ticks: nil),
+            track1ClipEndTime:   .barsAndBeats(bar: 20, beat: 1, ticks: nil),
+            track1ClipDuration:  .barsAndBeats(bar: 6, beat: 2, ticks: 720),
+            
+            track2ClipStartTime: .barsAndBeats(bar: 5, beat: 1, ticks: nil),
+            track2ClipEndTime:   .barsAndBeats(bar: 11, beat: 4, ticks: nil),
+            track2ClipDuration:  .barsAndBeats(bar: 6, beat: 2, ticks: 720),
+            
+            marker1Location:     .barsAndBeats(bar: 29, beat: 1, ticks: nil),
+            marker1TimeRef:      .samples(2695168),
+            
+            marker2Location:     .barsAndBeats(bar: 58, beat: 4, ticks: nil),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: nil)
         )
     }
     
     func testSessionText_BarsBeats_ShowSubframes() throws {
-        try runBarsBeats(
+        try runSessionText(
             filename: "SessionText_TimeFormats_BarsBeats_ShowSubframes_PT2022.9",
-            track1Clips: [
-                .init(
-                    channel: 1,
-                    event: 1,
-                    name: "Audio Clip 1 Name",
-                    startTime: .barsAndBeats(bar: 13, beat: 3, ticks: 48),
-                    endTime: .barsAndBeats(bar: 20, beat: 1, ticks: 768),
-                    duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
-                    state: .unmuted
-                )
-            ], track2Clips: [
-                .init(
-                    channel: 1,
-                    event: 1,
-                    name: "Audio Clip 2 Name",
-                    startTime: .barsAndBeats(bar: 5, beat: 1, ticks: 696),
-                    endTime: .barsAndBeats(bar: 11, beat: 4, ticks: 456),
-                    duration: .barsAndBeats(bar: 6, beat: 2, ticks: 720),
-                    state: .unmuted
-                )
-            ],
-            marker1Location: .barsAndBeats(bar: 29, beat: 1, ticks: 287),
-            marker1TimeRef: .samples(2695168),
-            marker2Location: .barsAndBeats(bar: 58, beat: 4, ticks: 735),
-            marker2TimeRef: .barsAndBeats(bar: 58, beat: 4, ticks: 735)
+            
+            track1ClipStartTime: .barsAndBeats(bar: 13, beat: 3, ticks: 48),
+            track1ClipEndTime:   .barsAndBeats(bar: 20, beat: 1, ticks: 768),
+            track1ClipDuration:  .barsAndBeats(bar: 6, beat: 2, ticks: 720),
+             
+            track2ClipStartTime: .barsAndBeats(bar: 5, beat: 1, ticks: 696),
+            track2ClipEndTime:   .barsAndBeats(bar: 11, beat: 4, ticks: 456),
+            track2ClipDuration:  .barsAndBeats(bar: 6, beat: 2, ticks: 720),
+            
+            marker1Location:     .barsAndBeats(bar: 29, beat: 1, ticks: 287),
+            marker1TimeRef:      .samples(2695168),
+            
+            marker2Location:     .barsAndBeats(bar: 58, beat: 4, ticks: 735),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: 735)
         )
     }
     
-    func runBarsBeats(
+    func testSessionText_FeetFrames() throws {
+        try runSessionText(
+            filename: "SessionText_TimeFormats_FeetFrames_PT2022.9",
+            
+            track1ClipStartTime: .feetAndFrames(feet: 37, frames: 8, subFrames: nil),
+            track1ClipEndTime:   .feetAndFrames(feet: 57, frames: 9, subFrames: nil),
+            track1ClipDuration:  .feetAndFrames(feet: 20, frames: 1, subFrames: nil),
+            
+            track2ClipStartTime: .feetAndFrames(feet: 12, frames: 8, subFrames: nil),
+            track2ClipEndTime:   .feetAndFrames(feet: 32, frames: 9, subFrames: nil),
+            track2ClipDuration:  .feetAndFrames(feet: 20, frames: 1, subFrames: nil),
+            
+            marker1Location:     .feetAndFrames(feet: 84, frames: 3, subFrames: nil),
+            marker1TimeRef:      .samples(2695168),
+            
+            marker2Location:     .feetAndFrames(feet: 173, frames: 13, subFrames: nil),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: nil)
+        )
+    }
+    
+    func testSessionText_FeetFrames_ShowSubframes() throws {
+        try runSessionText(
+            filename: "SessionText_TimeFormats_FeetFrames_ShowSubframes_PT2022.9",
+            
+            track1ClipStartTime: .feetAndFrames(feet: 37, frames: 8, subFrames: 60),
+            track1ClipEndTime:   .feetAndFrames(feet: 57, frames: 9, subFrames: 60),
+            track1ClipDuration:  .feetAndFrames(feet: 20, frames: 1, subFrames: 00),
+            
+            track2ClipStartTime: .feetAndFrames(feet: 12, frames: 8, subFrames: 70),
+            track2ClipEndTime:   .feetAndFrames(feet: 32, frames: 9, subFrames: 70),
+            track2ClipDuration:  .feetAndFrames(feet: 20, frames: 1, subFrames: 00),
+            
+            marker1Location:     .feetAndFrames(feet: 84, frames: 3, subFrames: 58),
+            marker1TimeRef:      .samples(2695168),
+             
+            marker2Location:     .feetAndFrames(feet: 173, frames: 13, subFrames: 18),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: 735)
+        )
+    }
+    
+    func testSessionText_MinSecs() throws {
+        try runSessionText(
+            filename: "SessionText_TimeFormats_MinSecs_PT2022.9",
+            
+            track1ClipStartTime: .minSecs(min: 0, sec: 25, ms: nil),
+            track1ClipEndTime:   .minSecs(min: 0, sec: 38, ms: nil),
+            track1ClipDuration:  .minSecs(min: 0, sec: 13, ms: nil),
+            
+            track2ClipStartTime: .minSecs(min: 0, sec: 08, ms: nil),
+            track2ClipEndTime:   .minSecs(min: 0, sec: 21, ms: nil),
+            track2ClipDuration:  .minSecs(min: 0, sec: 13, ms: nil),
+            
+            marker1Location:     .minSecs(min: 0, sec: 56, ms: nil),
+            marker1TimeRef:      .samples(2695168),
+            
+            marker2Location:     .minSecs(min: 1, sec: 55, ms: nil),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: nil)
+        )
+    }
+    
+    func testSessionText_MinSecs_ShowSubframes() throws {
+        try runSessionText(
+            filename: "SessionText_TimeFormats_MinSecs_ShowSubframes_PT2022.9",
+            
+            track1ClipStartTime: .minSecs(min: 0, sec: 25, ms: 025),
+            track1ClipEndTime:   .minSecs(min: 0, sec: 38, ms: 400),
+            track1ClipDuration:  .minSecs(min: 0, sec: 13, ms: 375),
+            
+            track2ClipStartTime: .minSecs(min: 0, sec: 08, ms: 362),
+            track2ClipEndTime:   .minSecs(min: 0, sec: 21, ms: 737),
+            track2ClipDuration:  .minSecs(min: 0, sec: 13, ms: 375),
+            
+            marker1Location:     .minSecs(min: 0, sec: 56, ms: 149),
+            marker1TimeRef:      .samples(2_695_168),
+            
+            marker2Location:     .minSecs(min: 1, sec: 55, ms: 882),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: 735)
+        )
+    }
+    
+    func testSessionText_Samples() throws {
+        try runSessionText(
+            filename: "SessionText_TimeFormats_Samples_PT2022.9",
+            
+            track1ClipStartTime: .samples(1201200),
+            track1ClipEndTime:   .samples(1843200),
+            track1ClipDuration:  .samples(642000),
+            
+            track2ClipStartTime: .samples(401408),
+            track2ClipEndTime:   .samples(1043408),
+            track2ClipDuration:  .samples(642000),
+            
+            marker1Location:     .samples(2695168),
+            marker1TimeRef:      .samples(2695168),
+            
+            marker2Location:     .samples(5562368),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: nil)
+        )
+    }
+    
+    func testSessionText_Samples_ShowSubframes() throws {
+        try runSessionText(
+            filename: "SessionText_TimeFormats_Samples_ShowSubframes_PT2022.9",
+            
+            track1ClipStartTime: .samples(1201200),
+            track1ClipEndTime:   .samples(1843200),
+            track1ClipDuration:  .samples(642000),
+            
+            track2ClipStartTime: .samples(401408),
+            track2ClipEndTime:   .samples(1043408),
+            track2ClipDuration:  .samples(642000),
+            
+            marker1Location:     .samples(2695168),
+            marker1TimeRef:      .samples(2695168),
+            
+            marker2Location:     .samples(5562368),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: 735)
+        )
+    }
+    
+    func testSessionText_Timecode() throws {
+        func TC(_ tcc: TCC) -> Timecode {
+            try! ProTools.formTimecode(tcc, at: ._23_976)
+        }
+        try runSessionText(
+            filename: "SessionText_TimeFormats_Timecode_PT2022.9",
+            
+            track1ClipStartTime: .timecode(TC(TCC(h: 23, m: 57, s: 25, f: 00))),
+            track1ClipEndTime:   .timecode(TC(TCC(h: 23, m: 57, s: 38, f: 08))),
+            track1ClipDuration:  .timecode(TC(TCC(h: 00, m: 00, s: 13, f: 08))),
+            
+            track2ClipStartTime: .timecode(TC(TCC(h: 23, m: 57, s: 08, f: 08))),
+            track2ClipEndTime:   .timecode(TC(TCC(h: 23, m: 57, s: 21, f: 17))),
+            track2ClipDuration:  .timecode(TC(TCC(h: 00, m: 00, s: 13, f: 08))),
+            
+            marker1Location:     .timecode(TC(TCC(h: 23, m: 57, s: 56, f: 02))),
+            marker1TimeRef:      .samples(2695168),
+            
+            marker2Location:     .timecode(TC(TCC(h: 23, m: 58, s: 55, f: 18))),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: nil)
+        )
+    }
+    
+    func testSessionText_Timecode_ShowSubframes() throws {
+        func TC(_ tcc: TCC) -> Timecode {
+            try! ProTools.formTimecode(tcc, at: ._23_976)
+        }
+        try runSessionText(
+            filename: "SessionText_TimeFormats_Timecode_ShowSubframes_PT2022.9",
+            
+            track1ClipStartTime: .timecode(TC(TCC(h: 23, m: 57, s: 25, f: 00, sf: 00))),
+            track1ClipEndTime:   .timecode(TC(TCC(h: 23, m: 57, s: 38, f: 08, sf: 68))),
+            track1ClipDuration:  .timecode(TC(TCC(h: 00, m: 00, s: 13, f: 08, sf: 68))),
+            
+            track2ClipStartTime: .timecode(TC(TCC(h: 23, m: 57, s: 08, f: 08, sf: 50))),
+            track2ClipEndTime:   .timecode(TC(TCC(h: 23, m: 57, s: 21, f: 17, sf: 18))),
+            track2ClipDuration:  .timecode(TC(TCC(h: 00, m: 00, s: 13, f: 08, sf: 68))),
+            
+            marker1Location:     .timecode(TC(TCC(h: 23, m: 57, s: 56, f: 02, sf: 24))),
+            marker1TimeRef:      .samples(2695168),
+            
+            marker2Location:     .timecode(TC(TCC(h: 23, m: 58, s: 55, f: 18, sf: 41))),
+            marker2TimeRef:      .barsAndBeats(bar: 58, beat: 4, ticks: 735)
+        )
+    }
+    
+    // MARK: - Common Test Body
+    
+    func runSessionText(
         filename: String,
-        track1Clips: [ProTools.SessionInfo.Track.Clip],
-        track2Clips: [ProTools.SessionInfo.Track.Clip],
+        track1ClipStartTime: ProTools.SessionInfo.TimeValue,
+        track1ClipEndTime: ProTools.SessionInfo.TimeValue,
+        track1ClipDuration: ProTools.SessionInfo.TimeValue?,
+        track2ClipStartTime: ProTools.SessionInfo.TimeValue,
+        track2ClipEndTime: ProTools.SessionInfo.TimeValue,
+        track2ClipDuration: ProTools.SessionInfo.TimeValue?,
         marker1Location: ProTools.SessionInfo.TimeValue,
         marker1TimeRef: ProTools.SessionInfo.TimeValue,
         marker2Location: ProTools.SessionInfo.TimeValue,
@@ -148,7 +298,17 @@ class ProTools_SessionText_TimeFormats_BarsBeats: XCTestCase {
         XCTAssertEqual(track1.userDelay, 0)
         XCTAssertEqual(track1.state, [])
         XCTAssertEqual(track1.plugins, [])
-        XCTAssertEqual(track1.clips, track1Clips)
+        XCTAssertEqual(track1.clips, [
+            .init(
+                channel: 1,
+                event: 1,
+                name: "Audio Clip 1 Name",
+                startTime: track1ClipStartTime,
+                endTime: track1ClipEndTime,
+                duration: track1ClipDuration,
+                state: .unmuted
+            )
+        ])
         
         let track2 = try XCTUnwrap(tracks[safe: 1])
         XCTAssertEqual(track2.name, "Audio B")
@@ -156,7 +316,17 @@ class ProTools_SessionText_TimeFormats_BarsBeats: XCTestCase {
         XCTAssertEqual(track2.userDelay, 0)
         XCTAssertEqual(track2.state, [])
         XCTAssertEqual(track2.plugins, [])
-        XCTAssertEqual(track2.clips, track2Clips)
+        XCTAssertEqual(track2.clips, [
+            .init(
+                channel: 1,
+                event: 1,
+                name: "Audio Clip 2 Name",
+                startTime: track2ClipStartTime,
+                endTime: track2ClipEndTime,
+                duration: track2ClipDuration,
+                state: .unmuted
+            )
+        ])
         
         // markers
         
