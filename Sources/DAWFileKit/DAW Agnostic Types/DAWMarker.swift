6c1f7088b969621e9aaff8d97d906433d1078b85
Steffan Andrews <stef@orchetect.com>
2022-10-29 16:56:50 -0700

    Updated inline docs

--- Sources/DAWFileKit/DAW Agnostic Types/DAWMarker.swift
+++ Sources/DAWFileKit/DAW Agnostic Types/DAWMarker.swift
@@ -11,16 +11,12 @@ public struct DAWMarker: Codable {
     
     /// The core time value storage.
     /// Regardless of type, the value must always represent time elapsed from zero (00:00:00:00).
-    ///
-    /// If the marker was imported from a file, its original format is preserved here.
-    /// Conceivably, if the user edits or processes the data, the application can (and probably will) overwrite this with the new data format.
-    /// However this should remain intact in its original format as long as possible (while loaded, displayed, and saved back to a file) to retain as much precision as long as possible.
     public var timeStorage: Storage? = nil
     
     /// Main text of the marker.
     public var name: String = ""
     
-    /// Comment associated with marker. Not all DAWs support comments, mainly Pro Tools.
+    /// Comment associated with marker. Not all DAWs support comments; mainly Pro Tools.
     public var comment: String?
     
     // MARK: init
