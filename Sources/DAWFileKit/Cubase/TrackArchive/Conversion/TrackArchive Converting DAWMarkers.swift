178731fd65e3864331781a6ae1547dee559fd154
Steffan Andrews <stef@orchetect.com>
2022-10-29 17:36:08 -0700

    Fixed build on macCatalyst, iOS, tvOS

--- Sources/DAWFileKit/Cubase/TrackArchive/Conversion/TrackArchive Converting DAWMarkers.swift
+++ Sources/DAWFileKit/Cubase/TrackArchive/Conversion/TrackArchive Converting DAWMarkers.swift
@@ -4,6 +4,8 @@
 //  © 2022 Steffan Andrews • Licensed under MIT License
 //
 
+#if os(macOS) // XMLNode only works on macOS
+
 import Foundation
 @_implementationOnly import OTCore
 import TimecodeKit
@@ -132,3 +134,5 @@ extension Array where Element == DAWMarker {
         }
     }
 }
+
+#endif
