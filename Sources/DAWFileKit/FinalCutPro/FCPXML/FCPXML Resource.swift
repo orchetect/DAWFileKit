0074cf7086a7d266f05fc8fd410bf4af9a94af23
Steffan Andrews <stef@orchetect.com>
2023-04-28 01:07:08 -0700

    `FCPXML`: Added asset resource

--- Sources/DAWFileKit/FinalCutPro/FCPXML/FCPXML Resource.swift
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/FCPXML Resource.swift
@@ -14,6 +14,7 @@ extension FinalCutPro.FCPXML {
     
     /// Resource
     public enum Resource: Equatable, Hashable {
+        case asset(Asset)
         case effect(Effect)
         case format(Format)
     }
@@ -21,12 +22,91 @@ extension FinalCutPro.FCPXML {
 
 extension FinalCutPro.FCPXML.Resource {
     public enum ResourceType: String {
+        case asset
         case effect
         case format
     }
 }
 
 extension FinalCutPro.FCPXML.Resource {
+    public struct Asset: Equatable, Hashable {
+        public let name: String
+        public let uid: String
+        public let start: String
+        public let duration: String
+        public let hasAudio: Bool
+        public let hasVideo: Bool
+        public let format: String?
+        public let audioSources: Int
+        public let videoSources: Int
+        public let audioChannels: Int
+        public let audioRate: Int?
+        
+        internal init(
+            name: String,
+            uid: String,
+            start: String,
+            duration: String,
+            hasAudio: Bool,
+            hasVideo: Bool,
+            format: String?,
+            audioSources: Int,
+            videoSources: Int,
+            audioChannels: Int,
+            audioRate: Int?
+        ) {
+            self.name = name
+            self.uid = uid
+            self.start = start
+            self.duration = duration
+            self.hasAudio = hasAudio
+            self.hasVideo = hasVideo
+            self.format = format
+            self.audioSources = audioSources
+            self.videoSources = videoSources
+            self.audioChannels = audioChannels
+            self.audioRate = audioRate
+        }
+        
+        init(from xmlLeaf: XMLElement) {
+            name = xmlLeaf.attributeStringValue(forName: Attributes.name.rawValue) ?? ""
+            
+            uid = xmlLeaf.attributeStringValue(forName: Attributes.uid.rawValue) ?? ""
+            
+            start = xmlLeaf.attributeStringValue(forName: Attributes.start.rawValue) ?? ""
+            
+            duration = xmlLeaf.attributeStringValue(forName: Attributes.duration.rawValue) ?? ""
+            
+            hasAudio = xmlLeaf.attributeStringValue(forName: Attributes.hasAudio.rawValue) ?? "0" == "1"
+            
+            hasVideo = xmlLeaf.attributeStringValue(forName: Attributes.hasVideo.rawValue) ?? "0" == "1"
+            
+            format = xmlLeaf.attributeStringValue(forName: Attributes.format.rawValue)
+            
+            audioSources = Int(xmlLeaf.attributeStringValue(forName: Attributes.audioSources.rawValue) ?? "0") ?? 0
+            
+            videoSources = Int(xmlLeaf.attributeStringValue(forName: Attributes.videoSources.rawValue) ?? "0") ?? 0
+            
+            audioChannels = Int(xmlLeaf.attributeStringValue(forName: Attributes.audioChannels.rawValue) ?? "0") ?? 0
+            
+            audioRate = xmlLeaf.attributeStringValue(forName: Attributes.audioRate.rawValue)?.int
+        }
+        
+        public enum Attributes: String {
+            case name
+            case uid
+            case start
+            case duration
+            case hasAudio
+            case hasVideo
+            case format
+            case audioSources
+            case videoSources
+            case audioChannels
+            case audioRate
+        }
+    }
+    
     public struct Effect: Equatable, Hashable {
         let name: String
         let uid: String
