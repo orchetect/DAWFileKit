b274f47b90cc8cf3cd6fa591a506d82b9893962e
Steffan Andrews <stef@orchetect.com>
2023-04-28 01:44:30 -0700

    `FCPXML`: Added Video Clip parsing

--- Sources/DAWFileKit/FinalCutPro/FCPXML/FCPXML Sequence Clip.swift
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/FCPXML Sequence Clip.swift
@@ -15,7 +15,7 @@ extension FinalCutPro.FCPXML.Sequence {
     public enum ClipType: String {
         case assetClip
         case title
-//        case video
+        case video
         
         // TODO: add additional clip types
     }
@@ -24,7 +24,7 @@ extension FinalCutPro.FCPXML.Sequence {
     public enum Clip {
         case assetClip(AssetClip)
         case title(Title)
-//        case video(Video)
+        case video(Video)
         
         // TODO: add additional clip types
     }
@@ -178,7 +178,7 @@ extension FinalCutPro.FCPXML.Sequence.Clip.AssetClip {
     }
 }
 
-// MARK: - Title
+// MARK: - Title Clip
 
 extension FinalCutPro.FCPXML.Sequence.Clip {
     // <title ref="r2" offset="0s" name="Basic Title" start="0s" duration="1920919/30000s">
@@ -264,6 +264,86 @@ extension FinalCutPro.FCPXML.Sequence.Clip.Title {
     }
 }
 
+// MARK: - Video Clip
+
+extension FinalCutPro.FCPXML.Sequence.Clip {
+    // <video ref="r7" offset="869600/2500s" name="Clouds" start="3600s" duration="250300/2500s" role="Sample Role.Sample Role-1">
+    /// Video Clip.
+    public struct Video {
+        public let ref: String // resource ID
+        public let offset: Timecode
+        public let name: String
+        public let start: Timecode
+        public let duration: Timecode
+        public let role: String
+        
+        internal init(
+            ref: String,
+            offset: Timecode,
+            name: String,
+            start: Timecode,
+            duration: Timecode,
+            role: String
+        ) {
+            self.ref = ref
+            self.offset = offset
+            self.name = name
+            self.start = start
+            self.duration = duration
+            self.role = role
+        }
+    }
+}
+
+extension FinalCutPro.FCPXML.Sequence.Clip.Video {
+    /// Video clip XML Attributes.
+    public enum Attributes: String {
+        case ref // resource ID
+        case offset
+        case name
+        case start
+        case duration
+        case role
+    }
+    
+    internal init(
+        from xmlLeaf: XMLElement,
+        sequenceFrameRate frameRate: TimecodeFrameRate
+    ) {
+        // "ref"
+        ref = FinalCutPro.FCPXML.Sequence.Clip.getRef(from: xmlLeaf)
+        
+        // "offset"
+        offset = FinalCutPro.FCPXML.Sequence.Clip.getTimecode(
+            attribute: .offset,
+            from: xmlLeaf,
+            sequenceFrameRate: frameRate
+        )
+        
+        // "name"
+        name = FinalCutPro.FCPXML.Sequence.Clip.getName(from: xmlLeaf)
+        
+        // "start"
+        start = FinalCutPro.FCPXML.Sequence.Clip.getTimecode(
+            attribute: .start,
+            from: xmlLeaf,
+            sequenceFrameRate: frameRate
+        )
+        
+        // "duration"
+        duration = FinalCutPro.FCPXML.Sequence.Clip.getTimecode(
+            attribute: .duration,
+            from: xmlLeaf,
+            sequenceFrameRate: frameRate
+        )
+        
+        // "role"
+        role = xmlLeaf.attributeStringValue(forName: Attributes.role.rawValue) ?? ""
+    }
+}
+
+// MARK: - Clip Item
+
 extension FinalCutPro.FCPXML.Sequence.Clip {
     /// Items within clips.
     public enum ClipItem: String {
@@ -305,12 +385,12 @@ extension FinalCutPro.FCPXML.Sequence {
                     )
                     return .title(clip)
                     
-//                case .video:
-//                    let clip = Clip.Video(
-//                        from: childLeaf,
-//                        sequenceFrameRate: frameRate
-//                    )
-//                    return .video(clip)
+                case .video:
+                    let clip = Clip.Video(
+                        from: childLeaf,
+                        sequenceFrameRate: frameRate
+                    )
+                    return .video(clip)
                     
                 }
             } ?? []
