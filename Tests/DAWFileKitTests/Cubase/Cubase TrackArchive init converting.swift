146d7e046476c1e6b72adf7951a72511d8500b63
Steffan Andrews <stef@orchetect.com>
2023-10-18 12:38:04 -0700

    Updated unit tests

--- Tests/DAWFileKitTests/Cubase/Cubase TrackArchive init converting.swift
+++ Tests/DAWFileKitTests/Cubase/Cubase TrackArchive init converting.swift
@@ -33,6 +33,9 @@ class Cubase_TrackArchive_ConvertingDAWMarkers: XCTestCase {
         
         // build messages
         XCTAssertEqual(buildMessages.count, 0)
+        if !buildMessages.errors.isEmpty {
+            dump(buildMessages.errors)
+        }
         
         // main
         XCTAssertEqual(trackArchive.main.startTimecode, Timecode(.zero, at: .fps24))
@@ -77,6 +80,9 @@ class Cubase_TrackArchive_ConvertingDAWMarkers: XCTestCase {
         
         // build messages
         XCTAssertEqual(buildMessages.count, 0)
+        if !buildMessages.errors.isEmpty {
+            dump(buildMessages.errors)
+        }
         
         // main
         XCTAssertEqual(trackArchive.main.startTimecode, Timecode(.zero, at: .fps24))
@@ -103,6 +109,71 @@ class Cubase_TrackArchive_ConvertingDAWMarkers: XCTestCase {
         XCTAssertEqual(marker2.startTimecode, try Timecode(.components(s: 1), at: .fps24))
     }
     
+    func testConvertingDAWMarkers_IncludeComments_SeparateCommentsTrack_VariedComments() throws {
+        let dawMarkers: [DAWMarker] = [
+            DAWMarker(storage: .init(value: .realTime(relativeToStart: 0.5), frameRate: .fps24, base: .max80SubFrames), name: "Marker1", comment: nil),
+            DAWMarker(storage: .init(value: .realTime(relativeToStart: 1.0), frameRate: .fps24, base: .max80SubFrames), name: "Marker2", comment: "Comment2")
+        ]
+        
+        var buildMessages: [Cubase.TrackArchive.EncodeMessage] = []
+        let trackArchive = Cubase.TrackArchive(
+            converting: dawMarkers,
+            at: .fps24,
+            startTimecode: Timecode(.zero, at: .fps24),
+            includeComments: true,
+            separateCommentsTrack: true,
+            buildMessages: &buildMessages
+        )
+        
+        // build messages
+        XCTAssertEqual(buildMessages.count, 0)
+        if !buildMessages.errors.isEmpty {
+            dump(buildMessages.errors)
+        }
+        
+        // main
+        XCTAssertEqual(trackArchive.main.startTimecode, Timecode(.zero, at: .fps24))
+        XCTAssertEqual(trackArchive.main.frameRate, .fps24)
+        
+        // markers
+        
+        let tracks = try XCTUnwrap(trackArchive.tracks)
+        XCTAssertEqual(tracks.count, 2)
+        
+        // track 1 (markers)
+        
+        do {
+            let track1 = try XCTUnwrap(tracks[0] as? Cubase.TrackArchive.MarkerTrack)
+            XCTAssertEqual(track1.name, "Markers")
+            
+            XCTAssertEqual(track1.events.count, 2)
+            
+            let marker1 = track1.events[0]
+            XCTAssertEqual(marker1.name, "Marker1")
+            XCTAssertEqual(marker1.startRealTime, nil) // not stored since we're computing based on start timecode
+            XCTAssertEqual(marker1.startTimecode, try Timecode(.components(f: 12), at: .fps24))
+            
+            let marker2 = track1.events[1]
+            XCTAssertEqual(marker2.name, "Marker2")
+            XCTAssertEqual(marker2.startRealTime, nil) // not stored since we're computing based on start timecode
+            XCTAssertEqual(marker2.startTimecode, try Timecode(.components(s: 1), at: .fps24))
+        }
+        
+        // track 2 (comments)
+        
+        do {
+            let track2 = try XCTUnwrap(tracks[1] as? Cubase.TrackArchive.MarkerTrack)
+            XCTAssertEqual(track2.name, "Comments")
+            
+            XCTAssertEqual(track2.events.count, 1)
+            
+            let marker = track2.events[0]
+            XCTAssertEqual(marker.name, "Comment2")
+            XCTAssertEqual(marker.startRealTime, nil) // not stored since we're computing based on start timecode
+            XCTAssertEqual(marker.startTimecode, try Timecode(.components(s: 1), at: .fps24))
+        }
+    }
+    
     func testConvertingDAWMarkers_DoNotIncludeComments_WithComments() throws {
         let dawMarkers: [DAWMarker] = [
             DAWMarker(storage: .init(value: .realTime(relativeToStart: 0.5), frameRate: .fps24, base: .max80SubFrames), name: "Marker1", comment: nil),
@@ -121,6 +192,9 @@ class Cubase_TrackArchive_ConvertingDAWMarkers: XCTestCase {
         
         // build messages
         XCTAssertEqual(buildMessages.count, 0)
+        if !buildMessages.errors.isEmpty {
+            dump(buildMessages.errors)
+        }
         
         // main
         XCTAssertEqual(trackArchive.main.startTimecode, Timecode(.zero, at: .fps24))
@@ -165,6 +239,9 @@ class Cubase_TrackArchive_ConvertingDAWMarkers: XCTestCase {
         
         // build messages
         XCTAssertEqual(buildMessages.count, 0)
+        if !buildMessages.errors.isEmpty {
+            dump(buildMessages.errors)
+        }
         
         // main
         XCTAssertEqual(trackArchive.main.startTimecode, Timecode(.zero, at: .fps24))
@@ -209,6 +286,9 @@ class Cubase_TrackArchive_ConvertingDAWMarkers: XCTestCase {
         
         // build messages
         XCTAssertEqual(buildMessages.count, 0)
+        if !buildMessages.errors.isEmpty {
+            dump(buildMessages.errors)
+        }
         
         // main
         XCTAssertEqual(trackArchive.main.startTimecode, try Timecode(.components(h: 23), at: .fps24))
