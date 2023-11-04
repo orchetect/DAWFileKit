b969b846f6a52315acb5635fde87b4caf2866b64
Steffan Andrews <stef@orchetect.com>
2023-11-04 15:24:43 -0700

    Added `XMLParsableAttributesKey`

---
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/Protocols/XMLParsableAttributesKey.swift
@@ -0,0 +1,27 @@
+//
+//  XMLParsableAttributesKey.swift
+//  DAWFileKit • https://github.com/orchetect/DAWFileKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if os(macOS) // XMLNode only works on macOS
+
+import Foundation
+
+public protocol XMLParsableAttributesKey: Hashable, RawRepresentable, CaseIterable
+where RawValue == String { }
+
+extension XMLElement {
+    /// Utility to parse an XML element's attributes and return a key-value dictionary,
+    /// parsing only the keys contained in the `key` type passed.
+    /// Any missing keys will simply be omitted from the returned dictionary.
+    public func parseAttributesRawValues<K>(
+        key: K.Type
+    ) -> [K: String] where K: XMLParsableAttributesKey {
+        K.allCases.reduce(into: [:]) { dict, key in
+            dict[key] = attributeStringValue(forName: key.rawValue)
+        }
+    }
+}
+
+#endif
