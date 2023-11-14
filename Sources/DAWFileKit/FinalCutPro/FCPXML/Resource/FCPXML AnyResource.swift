9c809cbb76fd86f120bfd402dd664aabc0878ecd
Steffan Andrews <stef@orchetect.com>
2023-11-13 18:19:37 -0800

    FCPXML: Added `ResourceType` init from XML

--- Sources/DAWFileKit/FinalCutPro/FCPXML/Resource/FCPXML AnyResource.swift
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/Resource/FCPXML AnyResource.swift
@@ -23,15 +23,38 @@ extension FinalCutPro.FCPXML {
 }
 
 extension FinalCutPro.FCPXML.AnyResource {
-    /// Shared resource type.
-    public enum ResourceType: String {
-        case asset
-        case media
-        case format
-        case effect
-        case locator
-        case objectTracker = "object-tracker"
-        case trackingShape = "tracking-shape"
+    init?(
+        from xmlLeaf: XMLElement
+    ) {
+        guard let name = xmlLeaf.name else { return nil }
+        guard let resourceType = FinalCutPro.FCPXML.ResourceType(rawValue: name) else {
+            print("Unrecognized FCPXML resource type: \(name)")
+            return nil
+        }
+        
+        switch resourceType {
+        case .asset:
+            guard let res = FinalCutPro.FCPXML.Asset(from: xmlLeaf) else { return nil }
+            self = .asset(res)
+        case .media:
+            let res = FinalCutPro.FCPXML.Media(from: xmlLeaf)
+            self = .media(res)
+        case .format:
+            guard let res = FinalCutPro.FCPXML.Format(from: xmlLeaf) else { return nil }
+            self = .format(res)
+        case .effect:
+            guard let res = FinalCutPro.FCPXML.Effect(from: xmlLeaf) else { return nil }
+            self = .effect(res)
+        case .locator:
+            let res = FinalCutPro.FCPXML.Locator(from: xmlLeaf)
+            self = .locator(res)
+        case .objectTracker:
+            let res = FinalCutPro.FCPXML.ObjectTracker(from: xmlLeaf)
+            self = .objectTracker(res)
+        case .trackingShape:
+            let res = FinalCutPro.FCPXML.TrackingShape(from: xmlLeaf)
+            self = .trackingShape(res)
+        }
     }
 }
 
---
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/Resource/FCPXML ResourceType.swift
@@ -0,0 +1,24 @@
+//
+//  FCPXML ResourceType.swift
+//  DAWFileKit • https://github.com/orchetect/DAWFileKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if os(macOS) // XMLNode only works on macOS
+
+import Foundation
+
+extension FinalCutPro.FCPXML {
+    /// Shared resource type.
+    public enum ResourceType: String, CaseIterable {
+        case asset
+        case media
+        case format
+        case effect
+        case locator
+        case objectTracker = "object-tracker"
+        case trackingShape = "tracking-shape"
+    }
+}
+
+#endif
