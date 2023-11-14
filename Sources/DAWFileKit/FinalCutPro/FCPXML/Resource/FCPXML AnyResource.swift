ad90866dd1c79bbc925eeb0821205cfb6d300ecd
Steffan Andrews <stef@orchetect.com>
2023-11-13 21:03:24 -0800

    `FCPXML.AnyResource`: Added Collection `contains` methods

--- Sources/DAWFileKit/FinalCutPro/FCPXML/Resource/FCPXML AnyResource.swift
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/Resource/FCPXML AnyResource.swift
@@ -58,4 +58,88 @@ extension FinalCutPro.FCPXML.AnyResource {
     }
 }
 
+// MARK: - Collection Methods
+// TODO: refactor using new `FCPXMLResource` protocol and generics
+
+extension Collection<FinalCutPro.FCPXML.AnyResource> {
+    public func contains(_ resource: FinalCutPro.FCPXML.Asset) -> Bool {
+        contains(where: { element in
+            if case let .asset(rsc) = element { return resource == rsc }
+            return false
+        })
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.Media) -> Bool {
+        contains(where: { element in
+            if case let .media(rsc) = element { return resource == rsc }
+            return false
+        })
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.Format) -> Bool {
+        contains(where: { element in
+            if case let .format(rsc) = element { return resource == rsc }
+            return false
+        })
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.Effect) -> Bool {
+        contains(where: { element in
+            if case let .effect(rsc) = element { return resource == rsc }
+            return false
+        })
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.Locator) -> Bool {
+        contains(where: { element in
+            if case let .locator(rsc) = element { return resource == rsc }
+            return false
+        })
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.ObjectTracker) -> Bool {
+        contains(where: { element in
+            if case let .objectTracker(rsc) = element { return resource == rsc }
+            return false
+        })
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.TrackingShape) -> Bool {
+        contains(where: { element in
+            if case let .trackingShape(rsc) = element { return resource == rsc }
+            return false
+        })
+    }
+}
+
+extension Dictionary where Value == FinalCutPro.FCPXML.AnyResource {
+    public func contains(_ resource: FinalCutPro.FCPXML.Asset) -> Bool {
+        values.contains(resource)
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.Media) -> Bool {
+        values.contains(resource)
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.Format) -> Bool {
+        values.contains(resource)
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.Effect) -> Bool {
+        values.contains(resource)
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.Locator) -> Bool {
+        values.contains(resource)
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.ObjectTracker) -> Bool {
+        values.contains(resource)
+    }
+    
+    public func contains(_ resource: FinalCutPro.FCPXML.TrackingShape) -> Bool {
+        values.contains(resource)
+    }
+}
+
 #endif
