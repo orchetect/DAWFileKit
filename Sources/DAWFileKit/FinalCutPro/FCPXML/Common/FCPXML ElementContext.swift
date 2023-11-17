9943fd30e7adce89e33261cf60ba87e72f0e31af
Steffan Andrews <stef@orchetect.com>
2023-11-16 17:31:21 -0800

    Cleanup

--- Sources/DAWFileKit/FinalCutPro/FCPXML/Common/FCPXML ElementContext.swift
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/Common/FCPXML ElementContext.swift
@@ -47,82 +47,104 @@ extension FinalCutPro.FCPXML {
             
             parentType = parent.storyElementType
             parentName = parent.name
-            parentAbsoluteStart = Self.aggregateOffset(parent: parent, ancestorsOfParent: ancestorsOfParent)
+            parentAbsoluteStart = Self.aggregateOffset(
+                parent: parent,
+                ancestorsOfParent: ancestorsOfParent
+            )
             parentDuration = parent.duration
             
-            // calculate absolute start
-            
-            let parentStart = Self.nearestStart(parent: parent, ancestorsOfParent: ancestorsOfParent)
-            
-            if let elementStart = element.start,
-               let parentStart = parentStart,
-               let parentAbsoluteStart = parentAbsoluteStart
-            {
-                let localElementStart = elementStart - parentStart
-                if let elementAbsoluteStart = try? parentAbsoluteStart.adding(localElementStart, by: .wrapping) {
-                    absoluteStart = elementAbsoluteStart
-                } else {
-                    print("Error offsetting timecode for element \(element.name?.quoted ?? "").")
-                }
-            } else {
-                let pas = parentAbsoluteStart?.stringValue(format: [.showSubFrames]) ?? "missing"
-                let ps = parentStart?.stringValue(format: [.showSubFrames]) ?? "missing"
-                print(
-                    "Error calculating absolute timecode for element \(element.name?.quoted ?? "")."
+            absoluteStart = Self.calculateAbsoluteStart(
+                element: element, 
+                parent: parent,
+                ancestorsOfParent: ancestorsOfParent,
+                parentAbsoluteStart: parentAbsoluteStart
+            )
+        }
+    }
+}
+
+extension FinalCutPro.FCPXML.ElementContext {
+    private static func calculateAbsoluteStart<Element: FCPXMLExtractableElement>(
+        element: Element,
+        parent: FinalCutPro.FCPXML.AnyStoryElement,
+        ancestorsOfParent: [FinalCutPro.FCPXML.AnyStoryElement],
+        parentAbsoluteStart: Timecode?
+    ) -> Timecode? {
+        let parentStart = nearestStart(parent: parent, ancestorsOfParent: ancestorsOfParent)
+        
+        guard let elementStart = element.start,
+              let parentStart = parentStart,
+              let parentAbsoluteStart = parentAbsoluteStart
+        else {
+            let pas = parentAbsoluteStart?.stringValue(format: [.showSubFrames]) ?? "missing"
+            let ps = parentStart?.stringValue(format: [.showSubFrames]) ?? "missing"
+            print(
+                "Error calculating absolute timecode for element \(element.name?.quoted ?? "")."
                     + " Parent absolute start: \(pas) Parent start: \(ps)"
-                )
-            }
-        }
-        
-        /// Return absolute timecode of innermost parent by calculating aggregate offset of ancestors.
-        /// - Note: Ancestors is ordered from furthest ancestor to closest ancestor of the `parent`.
-        private static func aggregateOffset(
-            parent: AnyStoryElement,
-            ancestorsOfParent: [AnyStoryElement]
-        ) -> Timecode? {
-            let ancestors = ancestorsOfParent + [parent] // topmost -> innermost
-            
-            var pos: Timecode?
-            
-            func add(_ other: Timecode?) {
-                guard let other = other else { return }
-                let newTC = pos ?? Timecode(.zero, using: other.properties)
-                pos = try? newTC.adding(other, by: .wrapping)
-            }
-            
-            for ancestor in ancestors {
-                switch ancestor {
-                case let .anyClip(clip):
-                    add(clip.offset)
-                    
-                case .sequence(_ /* let sequence */):
-                    // pos = sequence.startTimecode
-                    break
-                    
-                case let .spine(spine):
-                    add(spine.offset)
-                }
-                
-            }
-            
-            return pos
-        }
-        
-        /// Return nearest `start` attribute value, starting from closest parent and traversing up
-        /// through ancestors.
-        /// - Note: Ancestors is ordered from furthest ancestor to closest ancestor of the `parent`.
-        private static func nearestStart(
-            parent: AnyStoryElement,
-            ancestorsOfParent: [AnyStoryElement]
-        ) -> Timecode? {
-            let ancestors = ancestorsOfParent + [parent] // topmost -> innermost
-            
-            for ancestor in ancestors.reversed() {
-                if let start = ancestor.start { return start }
-            }
-            
+            )
             return nil
         }
+        
+        let localElementStart = elementStart - parentStart
+        guard let elementAbsoluteStart = try? parentAbsoluteStart.adding(
+            localElementStart,
+            by: .wrapping
+        )
+        else {
+            print("Error offsetting timecode for element \(element.name?.quoted ?? "").")
+            return nil
+        }
+        
+        return elementAbsoluteStart
+    }
+    
+    /// Return absolute timecode of innermost parent by calculating aggregate offset of ancestors.
+    /// - Note: Ancestors is ordered from furthest ancestor to closest ancestor of the `parent`.
+    private static func aggregateOffset(
+        parent: FinalCutPro.FCPXML.AnyStoryElement,
+        ancestorsOfParent: [FinalCutPro.FCPXML.AnyStoryElement]
+    ) -> Timecode? {
+        let ancestors = ancestorsOfParent + [parent] // topmost -> innermost
+        
+        var pos: Timecode?
+        
+        func add(_ other: Timecode?) {
+            guard let other = other else { return }
+            let newTC = pos ?? Timecode(.zero, using: other.properties)
+            pos = try? newTC.adding(other, by: .wrapping)
+        }
+        
+        for ancestor in ancestors {
+            switch ancestor {
+            case let .anyClip(clip):
+                add(clip.offset)
+                
+            case .sequence(_ /* let sequence */ ):
+                // pos = sequence.startTimecode
+                break
+                
+            case let .spine(spine):
+                add(spine.offset)
+            }
+        }
+        
+        return pos
+    }
+    
+    /// Return nearest `start` attribute value, starting from closest parent and traversing up
+    /// through ancestors.
+    /// - Note: Ancestors is ordered from furthest ancestor to closest ancestor of the `parent`.
+    private static func nearestStart(
+        parent: FinalCutPro.FCPXML.AnyStoryElement,
+        ancestorsOfParent: [FinalCutPro.FCPXML.AnyStoryElement]
+    ) -> Timecode? {
+        let ancestors = ancestorsOfParent + [parent] // topmost -> innermost
+        
+        for ancestor in ancestors.reversed() {
+            if let start = ancestor.start { return start }
+        }
+        
+        return nil
     }
 }
 
