7856a9e9919b300ff0bc356a462a34d803112f8d
Steffan Andrews <stef@orchetect.com>
2023-11-28 01:50:00 -0800

    Removed obsolete LinuxMain test file

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-//
-//  LinuxMain.swift
-//  DAWFileKit • https://github.com/orchetect/DAWFileKit
-//  © 2022 Steffan Andrews • Licensed under MIT License
-//
-
-fatalError("Run the tests with `swift test --enable-test-discovery`")
