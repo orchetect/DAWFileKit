f811fa2ea05a758a6dedb4782d2e6a1345d81d9e
Steffan Andrews <stef@orchetect.com>
2023-12-01 16:10:06 -0800

    FCPXML: Added root model objects

---
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/Model/FCPXML Root.swift
@@ -0,0 +1,60 @@
+//
+//  FCPXML.swift
+//  DAWFileKit • https://github.com/orchetect/DAWFileKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if os(macOS) // XMLNode only works on macOS
+
+import Foundation
+@_implementationOnly import OTCore
+
+// MARK: - root/*
+
+extension FinalCutPro.FCPXML {
+    public enum RootChildren: String {
+        case fcpxml
+    }
+}
+
+// MARK: - root/fcpxml/*
+
+extension FinalCutPro.FCPXML {
+    public enum Attributes: String {
+        case version
+    }
+    
+    public enum Children: String {
+        case resources // structure element
+        case library // structure element
+        case event // structure element
+        case project // structure element
+        
+        // AFAIK it's possible to have clips here too
+    }
+}
+
+extension FinalCutPro.FCPXML {
+    /// Utility:
+    /// Returns the root `fcpxml` XML element if it exists.
+    public var fcpxmlElement: XMLElement? {
+        xml.childElements
+            .first(whereNodeNamed: RootChildren.fcpxml.rawValue)
+    }
+    
+    /// Utility:
+    /// Returns the `fcpxml/resources` XML element if it exists.
+    /// Exactly one of these elements is always required, regardless of the version of the FCPXML.
+    public var resourcesElement: XMLElement? {
+        fcpxmlElement?.firstChildElement(named: Children.resources.rawValue)
+    }
+    
+    /// Utility:
+    /// Returns the `fcpxml/library` XML element if it exists.
+    /// One or zero of these elements may be present within the `fcpxml` element.
+    public var libraryElement: XMLElement? {
+        fcpxmlElement?.firstChildElement(named: Children.library.rawValue)
+    }
+}
+
+#endif
---
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/Model/FCPXML Version.swift
@@ -0,0 +1,38 @@
+//
+//  FCPXML Version.swift
+//  DAWFileKit • https://github.com/orchetect/DAWFileKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if os(macOS) // XMLNode only works on macOS
+
+extension FinalCutPro.FCPXML {
+    /// FCPXML format version.
+    public enum Version: String {
+        case ver1_0 = "1.0"
+        case ver1_1 = "1.1"
+        case ver1_2 = "1.2"
+        case ver1_3 = "1.3"
+        case ver1_4 = "1.4"
+        case ver1_5 = "1.5"
+        case ver1_6 = "1.6"
+        case ver1_7 = "1.7"
+        case ver1_8 = "1.8"
+        case ver1_9 = "1.9"
+        case ver1_10 = "1.10"
+        case ver1_11 = "1.11"
+    }
+}
+
+extension FinalCutPro.FCPXML {
+    /// Returns the FCPXML format version.
+    public var version: Version? {
+        guard let verString = fcpxmlElement?
+            .stringValue(forAttributeNamed: Attributes.version.rawValue)
+        else { return nil }
+        
+        return Version(rawValue: verString)
+    }
+}
+
+#endif
