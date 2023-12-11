2d766b5e4dccf85ce539b0850dc5eb443ee2062d
Steffan Andrews <stef@orchetect.com>
2023-12-10 19:34:41 -0800

    FCPXML: Added unit test

--- Tests/DAWFileKitTests/FinalCutPro/File Tests/FinalCutPro FCPXML Occlusion.swift
+++ Tests/DAWFileKitTests/FinalCutPro/File Tests/FinalCutPro FCPXML Occlusion.swift
@@ -25,128 +25,167 @@ final class FinalCutPro_FCPXML_Occlusion: FCPXMLTestCase {
         ))
     } }
     
-//    func testParseAndOcclusion() throws {
-//        // load file
-//        let rawData = try fileContents
-//        
-//        // parse file
-//        let fcpxml = try FinalCutPro.FCPXML(fileContent: rawData)
-//        
-//        // resources
-//        // let resources = fcpxml.resources()
-//        
-//        // events
-//        let events = fcpxml.allEvents()
-//        XCTAssertEqual(events.count, 1)
-//        
-//        let event = try XCTUnwrap(events[safe: 0])
-//        XCTAssertEqual(event.name, "Test Event")
-//        // XCTAssertEqual(event.context[.occlusion], .notOccluded)
-//        
-//        // projects
-//        let projects = event.projects
-//        XCTAssertEqual(projects.count, 1)
-//        
-//        let project = try XCTUnwrap(projects.first)
-//        // XCTAssertEqual(project.context[.occlusion], .notOccluded)
-//        
-//        // sequence
-//        let sequence = project.sequence
-//        // XCTAssertEqual(sequence.context[.occlusion], .notOccluded)
-//        
-//        // spine
-//        let spine = sequence.spine
-//        XCTAssertEqual(spine.contents.count, 9)
-//        
-//        // story elements
-//        
-//        // title1 - 3 markers not occluded, 1 marker fully occluded
-//        
-//        guard case let .anyClip(.title(title1)) = spine.contents[0]
-//        else { XCTFail("Clip was not expected type.") ; return }
-//        
-//        XCTAssertEqual(title1.ref, "r2")
-//        XCTAssertEqual(title1.offset, Self.tc("00:00:00:00", .fps24))
-//        XCTAssertEqual(title1.offset?.frameRate, .fps24)
-//        XCTAssertEqual(title1.name, "Basic Title 1")
-//        XCTAssertEqual(title1.start, Self.tc("01:00:00:00", .fps24))
-//        XCTAssertEqual(title1.duration, Self.tc("00:00:30:00", .fps24))
-//        XCTAssertEqual(title1.duration?.frameRate, .fps24)
-//        XCTAssertEqual(title1.context[.absoluteStart], Self.tc("00:00:00:00", .fps24))
-//        XCTAssertEqual(title1.context[.occlusion], .notOccluded)
-//        
-//        let title1Markers = title1.contents.annotations().markers()
-//        XCTAssertEqual(title1Markers.count, 4)
-//        
-//        let title1M1 = try XCTUnwrap(title1Markers[safe: 0])
-//        XCTAssertEqual(title1M1.name, "Marker on Start")
-//        XCTAssertEqual(title1M1.context[.absoluteStart], Self.tc("00:00:00:00", .fps24))
-//        XCTAssertEqual(title1M1.context[.occlusion], .notOccluded)
-//        XCTAssertEqual(title1M1.context[.effectiveOcclusion], .notOccluded)
-//        
-//        let title1M2 = try XCTUnwrap(title1Markers[safe: 1])
-//        XCTAssertEqual(title1M2.name, "Marker in Middle")
-//        XCTAssertEqual(title1M2.context[.absoluteStart], Self.tc("00:00:15:00", .fps24))
-//        XCTAssertEqual(title1M2.context[.occlusion], .notOccluded)
-//        XCTAssertEqual(title1M2.context[.effectiveOcclusion], .notOccluded)
-//        
-//        let title1M3 = try XCTUnwrap(title1Markers[safe: 2])
-//        XCTAssertEqual(title1M3.name, "Marker 1 Frame Before End")
-//        XCTAssertEqual(title1M3.context[.absoluteStart], Self.tc("00:00:29:23", .fps24))
-//        XCTAssertEqual(title1M3.context[.occlusion], .notOccluded)
-//        XCTAssertEqual(title1M3.context[.effectiveOcclusion], .notOccluded)
-//        
-//        // this marker is not visible from the main timeline, FCP hides it
-//        let title1M4 = try XCTUnwrap(title1Markers[safe: 3])
-//        XCTAssertEqual(title1M4.name, "Marker on End")
-//        XCTAssertEqual(title1M4.context[.absoluteStart], Self.tc("00:00:30:00", .fps24))
-//        XCTAssertEqual(title1M4.context[.occlusion], .fullyOccluded)
-//        XCTAssertEqual(title1M4.context[.effectiveOcclusion], .fullyOccluded)
-//        
-//        // title2 - 1 marker not occluded, 2 markers fully occluded
-//        
-//        guard case let .anyClip(.title(title2)) = spine.contents[2]
-//        else { XCTFail("Clip was not expected type.") ; return }
-//        XCTAssertEqual(title2.ref, "r2")
-//        XCTAssertEqual(title2.offset, Self.tc("00:00:40:00", .fps24))
-//        XCTAssertEqual(title2.offset?.frameRate, .fps24)
-//        XCTAssertEqual(title2.name, "Basic Title 2")
-//        XCTAssertEqual(title2.start, Self.tc("01:00:10:00", .fps24))
-//        XCTAssertEqual(title2.duration, Self.tc("00:00:10:00", .fps24))
-//        XCTAssertEqual(title2.duration?.frameRate, .fps24)
-//        XCTAssertEqual(title2.context[.absoluteStart], Self.tc("00:00:40:00", .fps24))
-//        XCTAssertEqual(title2.context[.occlusion], .notOccluded)
-//        XCTAssertEqual(title2.context[.effectiveOcclusion], .notOccluded)
-//        
-//        let title2Markers = title2.contents.annotations().markers()
-//        XCTAssertEqual(title2Markers.count, 3)
-//        
-//        // this marker is not visible from the main timeline, FCP hides it
-//        let title2M1 = try XCTUnwrap(title2Markers[safe: 0])
-//        XCTAssertEqual(title2M1.name, "Marker Before Start")
-//        XCTAssertEqual(title2M1.context[.absoluteStart], Self.tc("00:00:30:00", .fps24))
-//        XCTAssertEqual(title2M1.context[.occlusion], .fullyOccluded)
-//        XCTAssertEqual(title2M1.context[.effectiveOcclusion], .fullyOccluded)
-//        
-//        let title2M2 = try XCTUnwrap(title2Markers[safe: 1])
-//        XCTAssertEqual(title2M2.name, "Marker in Middle")
-//        XCTAssertEqual(title2M2.context[.absoluteStart], Self.tc("00:00:45:00", .fps24))
-//        XCTAssertEqual(title2M2.context[.occlusion], .notOccluded)
-//        XCTAssertEqual(title2M2.context[.effectiveOcclusion], .notOccluded)
-//        
-//        // this marker is not visible from the main timeline, FCP hides it
-//        let title2M3 = try XCTUnwrap(title2Markers[safe: 2])
-//        XCTAssertEqual(title2M3.name, "Marker Past End")
-//        XCTAssertEqual(title2M3.context[.absoluteStart], Self.tc("00:01:00:00", .fps24))
-//        XCTAssertEqual(title2M3.context[.occlusion], .fullyOccluded)
-//        XCTAssertEqual(title2M3.context[.effectiveOcclusion], .fullyOccluded)
-//        
-//        // refClip1 - contains 1 clip not occluded in media sequence or from main timeline
-//        
-//        guard case let .anyClip(.refClip(refClip1)) = spine.contents[safe: 4]
-//        else { XCTFail("Clip was not expected type.") ; return }
-//        XCTAssertEqual(refClip1.name, "Occlusion Clip 1")
-//        
+    func testParseAndOcclusion() throws {
+        // load file
+        let rawData = try fileContents
+        
+        // parse file
+        let fcpxml = try FinalCutPro.FCPXML(fileContent: rawData)
+        
+        // resources
+        // let resources = fcpxml.resources()
+        
+        // events
+        let events = fcpxml.allEvents()
+        XCTAssertEqual(events.count, 1)
+        
+        let event = try XCTUnwrap(events[safe: 0])
+        XCTAssertEqual(event.name, "Test Event")
+        let extractedEvent = event.element.fcpExtract()
+        XCTAssertEqual(extractedEvent.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedEvent.value(forContext: .effectiveOcclusion), .notOccluded)
+        
+        /// projects
+        let projects = event.projects.zeroIndexed
+        XCTAssertEqual(projects.count, 1)
+        
+        let project = try XCTUnwrap(projects[safe: 0])
+        let extractedProject = event.element.fcpExtract()
+        XCTAssertEqual(extractedProject.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedProject.value(forContext: .effectiveOcclusion), .notOccluded)
+        
+        let sequence = project.sequence
+        let extractedSequence = sequence.element.fcpExtract()
+        XCTAssertEqual(extractedSequence.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedSequence.value(forContext: .effectiveOcclusion), .notOccluded)
+        
+        // spine
+        let spine = sequence.spine
+        let storyElements = spine.storyElements.zeroIndexed
+        XCTAssertEqual(storyElements.count, 9)
+        
+        // story elements
+        
+        // title1 - 3 markers not occluded, 1 marker fully occluded
+        
+        let title1 = try XCTUnwrap(storyElements[safe: 0]?.fcpAsTitle)
+        XCTAssertEqual(title1.ref, "r2")
+        XCTAssertEqual(title1.offsetAsTimecode, Self.tc("00:00:00:00", .fps24))
+        XCTAssertEqual(title1.offsetAsTimecode?.frameRate, .fps24)
+        XCTAssertEqual(title1.name, "Basic Title 1")
+        XCTAssertEqual(title1.startAsTimecode, Self.tc("01:00:00:00", .fps24))
+        XCTAssertEqual(title1.durationAsTimecode, Self.tc("00:00:30:00", .fps24))
+        XCTAssertEqual(title1.durationAsTimecode?.frameRate, .fps24)
+        let extractedTitle1 = title1.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle1.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:00:00:00", .fps24)
+        )
+        XCTAssertEqual(extractedTitle1.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedTitle1.value(forContext: .effectiveOcclusion), .notOccluded)
+
+        let title1Markers = title1.storyElements.filter(whereFCPElement: .marker).zeroIndexed
+        XCTAssertEqual(title1Markers.count, 4)
+        
+        let title1M1 = try XCTUnwrap(title1Markers[safe: 0])
+        XCTAssertEqual(title1M1.name, "Marker on Start")
+        let extractedTitle1M1 = title1M1.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle1M1.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:00:00:00", .fps24)
+        )
+        XCTAssertEqual(extractedTitle1M1.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedTitle1M1.value(forContext: .effectiveOcclusion), .notOccluded)
+
+        let title1M2 = try XCTUnwrap(title1Markers[safe: 1])
+        XCTAssertEqual(title1M2.name, "Marker in Middle")
+        let extractedTitle1M2 = title1M2.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle1M2.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:00:15:00", .fps24)
+        )
+        XCTAssertEqual(extractedTitle1M2.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedTitle1M2.value(forContext: .effectiveOcclusion), .notOccluded)
+        
+        let title1M3 = try XCTUnwrap(title1Markers[safe: 2])
+        XCTAssertEqual(title1M3.name, "Marker 1 Frame Before End")
+        let extractedTitle1M3 = title1M3.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle1M3.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:00:29:23", .fps24)
+        )
+        XCTAssertEqual(extractedTitle1M3.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedTitle1M3.value(forContext: .effectiveOcclusion), .notOccluded)
+        
+        // this marker is not visible from the main timeline, FCP hides it
+        let title1M4 = try XCTUnwrap(title1Markers[safe: 3])
+        XCTAssertEqual(title1M4.name, "Marker on End")
+        let extractedTitle1M4 = title1M4.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle1M4.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:00:30:00", .fps24)
+        )
+        XCTAssertEqual(extractedTitle1M4.value(forContext: .occlusion), .fullyOccluded)
+        XCTAssertEqual(extractedTitle1M4.value(forContext: .effectiveOcclusion), .fullyOccluded)
+        
+        // title2 - 1 marker not occluded, 2 markers fully occluded
+        
+        let title2 = try XCTUnwrap(storyElements[safe: 2]?.fcpAsTitle)
+        XCTAssertEqual(title2.ref, "r2")
+        XCTAssertEqual(title2.offsetAsTimecode, Self.tc("00:00:40:00", .fps24))
+        XCTAssertEqual(title2.offsetAsTimecode?.frameRate, .fps24)
+        XCTAssertEqual(title2.name, "Basic Title 2")
+        XCTAssertEqual(title2.startAsTimecode, Self.tc("01:00:10:00", .fps24))
+        XCTAssertEqual(title2.durationAsTimecode, Self.tc("00:00:10:00", .fps24))
+        XCTAssertEqual(title2.durationAsTimecode?.frameRate, .fps24)
+        let extractedTitle2 = title2.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle2.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:00:40:00", .fps24))
+        XCTAssertEqual(extractedTitle2.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedTitle2.value(forContext: .effectiveOcclusion), .notOccluded)
+        
+        let title2Markers = title2.storyElements.filter(whereFCPElement: .marker).zeroIndexed
+        XCTAssertEqual(title2Markers.count, 3)
+        
+        // this marker is not visible from the main timeline, FCP hides it
+        let title2M1 = try XCTUnwrap(title2Markers[safe: 0])
+        XCTAssertEqual(title2M1.name, "Marker Before Start")
+        let extractedTitle2M1 = title2M1.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle2M1.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:00:30:00", .fps24)
+        )
+        XCTAssertEqual(extractedTitle2M1.value(forContext: .occlusion), .fullyOccluded)
+        XCTAssertEqual(extractedTitle2M1.value(forContext: .effectiveOcclusion), .fullyOccluded)
+        
+        let title2M2 = try XCTUnwrap(title2Markers[safe: 1])
+        XCTAssertEqual(title2M2.name, "Marker in Middle")
+        let extractedTitle2M2 = title2M2.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle2M2.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:00:45:00", .fps24)
+        )
+        XCTAssertEqual(extractedTitle2M2.value(forContext: .occlusion), .notOccluded)
+        XCTAssertEqual(extractedTitle2M2.value(forContext: .effectiveOcclusion), .notOccluded)
+
+        // this marker is not visible from the main timeline, FCP hides it
+        let title2M3 = try XCTUnwrap(title2Markers[safe: 2])
+        XCTAssertEqual(title2M3.name, "Marker Past End")
+        let extractedTitle2M3 = title2M3.element.fcpExtract()
+        XCTAssertEqual(
+            extractedTitle2M3.value(forContext: .absoluteStartAsTimecode),
+            Self.tc("00:01:00:00", .fps24)
+        )
+        XCTAssertEqual(extractedTitle2M3.value(forContext: .occlusion), .fullyOccluded)
+        XCTAssertEqual(extractedTitle2M3.value(forContext: .effectiveOcclusion), .fullyOccluded)
+
+        // refClip1 - contains 1 clip not occluded in media sequence or from main timeline
+        
+        let refClip1 = try XCTUnwrap(storyElements[safe: 4]?.fcpAsRefClip)
+        XCTAssertEqual(refClip1.name, "Occlusion Clip 1")
+        
+        #warning("> TODO: occlusion within a ref-clip must be tested with fcpExtractElements() and not fcpExtract()")
 //        let refClip1Sequence = refClip1.sequence
 //        XCTAssertEqual(refClip1Sequence.context[.occlusion], .notOccluded)
 //        XCTAssertEqual(refClip1Sequence.context[.effectiveOcclusion], .notOccluded)
@@ -185,7 +224,7 @@ final class FinalCutPro_FCPXML_Occlusion: FCPXMLTestCase {
 //        XCTAssertEqual(refClip3Title.name, "Basic Title 5")
 //        XCTAssertEqual(refClip3Title.context[.occlusion], .notOccluded) // in media sequence
 //        XCTAssertEqual(refClip3Title.context[.effectiveOcclusion], .fullyOccluded)
-//    }
+    }
     
     /// Test main timeline markers extraction with limited occlusion conditions.
     func testExtractMarkers_MainTimeline_LimitedOcclusions() throws {
@@ -199,6 +238,10 @@ final class FinalCutPro_FCPXML_Occlusion: FCPXMLTestCase {
         let event = try XCTUnwrap(fcpxml.allEvents().first)
         
         // extract markers
+        assert(
+            FinalCutPro.FCPXML.ExtractionSettings.mainTimeline.occlusions
+            == [.notOccluded, .partiallyOccluded]
+        )
         let extractedMarkers = event
             .extractElements(preset: .markers, settings: .mainTimeline)
             .zeroIndexed
