3c82e7576b2bd75ab48ec98ba08174d90f9025c0
Steffan Andrews <stef@orchetect.com>
2023-12-13 02:25:18 -0800

    Dependency branch switch

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/orchetect/OTCore", branch: "main"), //from: "1.4.16"),
-        .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.0.7"),
+        .package(url: "https://github.com/orchetect/OTCore", branch: "main"), //from: "1.4.17"),
+        .package(url: "https://github.com/orchetect/TimecodeKit", branch: "main"), // from: "2.0.8"),
         .package(url: "https://github.com/orchetect/MIDIKit.git", from: "0.9.2")
     ],
     
