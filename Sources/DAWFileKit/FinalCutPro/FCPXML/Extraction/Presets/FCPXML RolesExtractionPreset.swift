5920105de599b899d89654b515ee0f23945648a6
Steffan Andrews <stef@orchetect.com>
2023-12-14 22:45:41 -0800

    FCPXML: Added `RolesExtractionPreset`

---
+++ Sources/DAWFileKit/FinalCutPro/FCPXML/Extraction/Presets/FCPXML RolesExtractionPreset.swift
@@ -0,0 +1,54 @@
+//
+//  FCPXML RolesExtractionPreset.swift
+//  DAWFileKit • https://github.com/orchetect/DAWFileKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if os(macOS) // XMLNode only works on macOS
+
+import Foundation
+import OTCore
+
+extension FinalCutPro.FCPXML {
+    /// FCPXML extraction preset that extracts roles within a specified scope.
+    public struct RolesExtractionPreset: FCPXMLExtractionPreset {
+        var roleTypes: Set<RoleType>
+        
+        public init(
+            roleTypes: Set<RoleType>
+        ) {
+            self.roleTypes = roleTypes
+        }
+        
+        public func perform(
+            on extractable: XMLElement,
+            scope: FinalCutPro.FCPXML.ExtractionScope
+        ) async -> [FinalCutPro.FCPXML.AnyRole] {
+            let extracted = await extractable.fcpExtract(scope: scope) { element in
+                element
+                    .value(forContext: .inheritedRoles)
+                    .filter(roleTypes: roleTypes)
+                    .map(\.wrapped)
+            }
+            
+            let output = extracted
+                .flatMap { $0 }
+                .sortedByRoleType()
+            
+            return output
+        }
+    }
+}
+
+extension FCPXMLExtractionPreset where Self == FinalCutPro.FCPXML.RolesExtractionPreset {
+    /// FCPXML extraction preset that extracts roles within a specified scope.
+    public static func roles(
+        roleTypes: Set<FinalCutPro.FCPXML.RoleType> = .allCases
+    ) -> FinalCutPro.FCPXML.RolesExtractionPreset {
+        FinalCutPro.FCPXML.RolesExtractionPreset(
+            roleTypes: roleTypes
+        )
+    }
+}
+
+#endif
