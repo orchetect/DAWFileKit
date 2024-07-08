b9ad6fdf36b81c3a299fb07d4563ee9fe3a8eb9c
Steffan Andrews <stef@orchetect.com>
2024-07-07 19:53:47 -0700

    FCPXML: Added `FrameDataPreset` unit test

---
+++ Tests/DAWFileKitTests/FinalCutPro/Logic and Parsing/FinalCutPro FCPXML Frame Data Tests.swift
@@ -0,0 +1,194 @@
+//
+//  FinalCutPro FCPXML Frame Data Tests.swift
+//  DAWFileKit • https://github.com/orchetect/DAWFileKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if os(macOS) // XMLNode only works on macOS
+
+import XCTest
+@testable import DAWFileKit
+import OTCore
+import TimecodeKit
+
+final class FinalCutPro_FCPXML_FrameData: FCPXMLTestCase {
+    override func setUp() { }
+    override func tearDown() { }
+    
+    // MARK: - Test Data
+    
+    var fileContents: Data { get throws {
+        try XCTUnwrap(loadFileContents(
+            forResource: "ClipMetadata",
+            withExtension: "fcpxml",
+            subFolder: .fcpxmlExports
+        ))
+    } }
+    
+    func testFrameDataClips() async throws {
+        // load file
+        let rawData = try fileContents
+        
+        // parse file
+        let fcpxml = try FinalCutPro.FCPXML(fileContent: rawData)
+        
+        let timeline = try XCTUnwrap(fcpxml.allTimelines().first)
+        
+        let fd = await timeline.extract(preset: .frameData, scope: .mainTimeline)
+        
+        // debug
+        
+        print(fd.timelineStart.stringValue() + "\n----")
+        // dump(fd.clips)
+        print(
+            fd.clips
+                .map { "\($0.start) ..< \($0.end) \($0.clip.element.fcpName ?? "<unknown clip name>")" }
+                .joined(separator: "\n")
+        )
+        
+        // check extracted clips
+        
+        guard fd.clips.count == 3 else { XCTFail() ; return }
+        
+        let clip1 = fd.clips[0]
+        let clip2 = fd.clips[1]
+        let clip3 = fd.clips[2]
+        
+        XCTAssertEqual(clip1.start.components, .init(h: 1, m: 00, s: 00, f: 00))
+        XCTAssertEqual(clip1.end.components,   .init(h: 1, m: 01, s: 00, f: 00))
+        
+        XCTAssertEqual(clip2.start.components, .init(h: 1, m: 01, s: 00, f: 00))
+        XCTAssertEqual(clip2.end.components,   .init(h: 1, m: 01, s: 10, f: 01))
+        
+        XCTAssertEqual(clip3.start.components, .init(h: 1, m: 01, s: 10, f: 01))
+        XCTAssertEqual(clip3.end.components,   .init(h: 1, m: 01, s: 39, f: 14))
+    }
+    
+    func testFrameDataFrames() async throws {
+        // load file
+        let rawData = try fileContents
+        
+        // parse file
+        let fcpxml = try FinalCutPro.FCPXML(fileContent: rawData)
+        
+        let timeline = try XCTUnwrap(fcpxml.allTimelines().first)
+        
+        let fd = await timeline.extract(preset: .frameData, scope: .mainTimeline)
+        
+        // check individual timecodes (frames)
+        
+        do {
+            let tc = Self.tc("1:00:00:00", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "Clouds")
+            XCTAssertEqual(tcData.markers.count, 0)
+        }
+        
+        do {
+            let tc = Self.tc("1:00:02:10", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "Clouds")
+            XCTAssertEqual(tcData.markers.count, 0)
+        }
+        
+        do {
+            let tc = Self.tc("1:00:59:24", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "Clouds")
+            XCTAssertEqual(tcData.markers.count, 0)
+        }
+        
+        do {
+            let tc = Self.tc("1:00:59:24.79", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "Clouds")
+            XCTAssertEqual(tcData.markers.count, 0)
+        }
+        
+        do {
+            let tc = Self.tc("1:01:00:00", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "Basic Title")
+            XCTAssertEqual(tcData.markers.count, 0)
+        }
+        
+        do {
+            let tc = Self.tc("1:01:10:00.79", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "Basic Title")
+            XCTAssertEqual(tcData.markers.count, 0)
+        }
+        
+        do {
+            let tc = Self.tc("1:01:10:01", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "TestVideo")
+            XCTAssertEqual(tcData.markers.count, 0)
+        }
+        
+        do {
+            let tc = Self.tc("1:01:37:11", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "TestVideo")
+            XCTAssertEqual(tcData.markers.count, 1)
+            
+            let marker = try XCTUnwrap(tcData.markers.first)
+            XCTAssertEqual(marker.name, "Marker 1")
+        }
+        
+        do {
+            let tc = Self.tc("1:01:37:11.79", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "TestVideo")
+            XCTAssertEqual(tcData.markers.count, 1)
+            
+            let marker = try XCTUnwrap(tcData.markers.first)
+            XCTAssertEqual(marker.name, "Marker 1")
+        }
+        
+        do {
+            let tc = Self.tc("1:01:39:13.79", .fps25)
+            let _tcData = await fd.data(for: tc)
+            let tcData = try XCTUnwrap(_tcData)
+            
+            XCTAssertEqual(tcData.timecode, tc)
+            XCTAssertEqual(tcData.clipName, "TestVideo")
+            XCTAssertEqual(tcData.markers.count, 0)
+        }
+        
+        do {
+            let tc = Self.tc("1:01:39:14", .fps25)
+            let _tcData = await fd.data(for: tc)
+            XCTAssertNil(_tcData)
+        }
+    }
+}
+
+#endif
