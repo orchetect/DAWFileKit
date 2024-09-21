347be29c4fd6a3c03c5dac8ee320d3638b803161
Steffan Andrews <stef@orchetect.com>
2024-09-20 18:44:09 -0700

    Bumped dependency versions

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     
     dependencies: [
         .package(url: "https://github.com/orchetect/OTCore", from: "1.6.0"),
-        .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.0.11"),
+        .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.1.0"),
         .package(url: "https://github.com/orchetect/MIDIKit.git", from: "0.9.7")
     ],
     
