2fc33ef6db1c150c7ce0fa4e244fe7ad27e5ee4d
Steffan Andrews <stef@orchetect.com>
2024-09-20 17:55:01 -0700

    Bumped OTCore to 1.6.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/orchetect/OTCore", from: "1.5.3"),
+        .package(url: "https://github.com/orchetect/OTCore", from: "1.6.0"),
         .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.0.11"),
         .package(url: "https://github.com/orchetect/MIDIKit.git", from: "0.9.7")
     ],
