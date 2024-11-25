8470cd7f9fa1e11c7800721b15cab5ad96097f49
Steffan Andrews <stef@orchetect.com>
2024-11-25 05:11:54 -0800

    Package.swift cleanup

--- Package.swift
+++ Package.swift
@@ -5,24 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "DAWFileKit",
-    
     platforms: [
         .macOS(.v10_15), .iOS(.v10), .tvOS(.v10), .watchOS(.v6)
     ],
-    
     products: [
-        .library(
-            name: "DAWFileKit",
-            targets: ["DAWFileKit"]
-        )
+        .library(name: "DAWFileKit", targets: ["DAWFileKit"])
     ],
-    
     dependencies: [
         .package(url: "https://github.com/orchetect/OTCore", from: "1.6.0"),
         .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.3.3"),
         .package(url: "https://github.com/orchetect/MIDIKit", from: "0.9.9")
     ],
-    
     targets: [
         .target(
             name: "DAWFileKit",
@@ -32,7 +25,6 @@ let package = Package(
                 .product(name: "MIDIKitSMF", package: "MIDIKit")
             ]
         ),
-        
         .testTarget(
             name: "DAWFileKitTests",
             dependencies: ["DAWFileKit"],
