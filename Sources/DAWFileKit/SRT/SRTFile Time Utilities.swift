beb7e4e9f930054489b576fb7f1532ae6e221553
Steffan Andrews <stef@orchetect.com>
2025-07-26 20:20:54 -0700

    `SRTFile`: Refactored timestamp parsing

--- Sources/DAWFileKit/SRT/SRTFile Time Utilities.swift
+++ Sources/DAWFileKit/SRT/SRTFile Time Utilities.swift
@@ -8,9 +8,24 @@ import Foundation
 import OTCore
 
 extension Time {
+    static func decodeRegExPattern(strict: Bool, matchLine: Bool) -> String {
+        let pattern = strict
+            ? #"(\d{2}):(\d{2}):(\d{2}),(\d{3})"#
+            : #"\s*(\d{1,3})\s*[:;]\s*(\d{1,3})\s*[:;]\s*(\d{1,3})\s*[\.,]\s*(\d{1,3})\s*"#
+        return matchLine
+            ? "^" + pattern + "$"
+            : pattern
+    }
+    
     /// Decode from SRT file timestamp format.
-    init?<S: StringProtocol>(srtEncodedString: S) {
-        let pattern = #"(\d{2}):(\d{2}):(\d{2}),(\d{3})"#
+    ///
+    /// - Parameters:
+    ///   - srtEncodedString: Timestamp string to parse.
+    ///   - strict: Enable strict parsing mode. When strict, only the "00:00:00,000" format is accepted.
+    ///     When non-strict, a very loose heuristic is used to allow for superfluous whitespace, the
+    ///     use of non-standard time separator characters and non-enforcement of character zero-padding.
+    init?<S: StringProtocol>(srtEncodedString: S, strict: Bool = false) {
+        let pattern = Self.decodeRegExPattern(strict: strict, matchLine: true)
         let matches = srtEncodedString.regexMatches(
             captureGroupsFromPattern: pattern,
             options: [],
@@ -20,7 +35,9 @@ extension Time {
         guard let h = matches[1]?.int,
               let m = matches[2]?.int,
               let s = matches[3]?.int,
-              let ms = matches[4]?.int
+              let msString = matches[4],
+              strict ? msString.count == 3 : (1 ... 3).contains(msString.count),
+              let ms = msString.int
         else { return nil }
         
         self.init(hours: h, minutes: m, seconds: s, milliseconds: ms)
@@ -29,11 +46,11 @@ extension Time {
     /// Encode to SRT file timestamp format.
     func srtEncodedString() -> String {
         hours.string(paddedTo: 2)
-        + ":"
-        + minutes.string(paddedTo: 2)
-        + ":"
-        + seconds.string(paddedTo: 2)
-        + ","
-        + milliseconds.string(paddedTo: 3)
+            + ":"
+            + minutes.string(paddedTo: 2)
+            + ":"
+            + seconds.string(paddedTo: 2)
+            + ","
+            + milliseconds.string(paddedTo: 3)
     }
 }
